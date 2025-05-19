9d6cdca271ddde6e8a987cff55ebe5e6fa7e610d
Ashen Gamage <ashengamage223@gmail.com>
2025-05-19 12:50:57 +0530

    innit

--- src/main/java/controller/DoctorServlet.java
+++ src/main/java/controller/DoctorServlet.java
@@ -28,6 +28,7 @@ public class DoctorServlet extends HttpServlet {
     private static final String AVAILABILITY_FILE = "/data/doctors_availability.txt";
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
+
     private AppointmentService appointmentService;
     private DoctorAvailabilityService availabilityService;
     private FileHandler doctorFileHandler;
